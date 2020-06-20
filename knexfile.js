7160ddba79c12101cb6ff4cb84a9126b1cf81348
Kenneth-Brandon <onegiantsfan25@yahoo.com>
2020-06-19 23:32:40 -0700

    one more time

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,3 @@
-const pgConnection =
-  process.env.DATABASE_URL ||
-  'postgres://agulijecrvvuhk:e530156534f49a7b6a0374aa86921674b065765139535368d6a79ccbb02453a8@ec2-50-17-178-87.compute-1.amazonaws.com:5432/db56j7ts84o56u';
-
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -30,7 +26,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: pgConnection,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
